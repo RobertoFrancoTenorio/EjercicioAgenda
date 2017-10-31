1bf21ebec21f1e459c0d507bdb3946713ce692d8
Roberto Franco <robertofrancotenorio2@gmail.com>
2017-10-31 10:59:07 -0600

    Carga Archivo

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,6 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Agenda {
     private LinkedList <Contacto> contactos;
@@ -43,5 +46,21 @@ public class Agenda {
             archivo.write("\n");
         }
         archivo.close();
+    }
+
+    public void cargar(String nomArch)throws FileNotFoundException
+    {
+        Scanner archivo= new Scanner(new File(nomArch));
+        while(archivo.hasNextLine()) {
+            String linea = archivo.nextLine();
+            //System.out.println("Contacto: " + linea);
+            String [] arreglo = linea.split(",");
+            /*System.out.println("Nom: " +arreglo[0]);
+            System.out.println("Dir: " + arreglo[1]);
+            System.out.println("Tel: " + arreglo[2]);*/
+            long tel = Long.parseLong(arreglo[2].trim());
+            Contacto nuevo= new Contacto(arreglo[0],arreglo[1],tel);
+            contactos.add(nuevo);
+        }
     }
 }
--- src/AplicacionTerminal.java
+++ src/AplicacionTerminal.java
@@ -1,4 +1,4 @@
-
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class AplicacionTerminal
 {
     private Agenda agenda;
-
     public AplicacionTerminal()
     {
         agenda = new Agenda();
@@ -14,85 +13,102 @@ public class AplicacionTerminal
 
     public void demo()
     {
-        Contacto c1= new Contacto("Roberto", ""  +   " uaslp", 1234567890);
-        Contacto c2= new Contacto("Carlos", ""   +   " UASLP", 1234567880);
-        Contacto c3= new Contacto("Franco", "" + " U.A.S.L.P", 1234566890);
-
-        //Agenda agenda = new Agenda();
-
-        agenda.agregaContacto(c1);
-        agenda.agregaContacto(c2);
-        agenda.agregaContacto(c3);
-        agenda.imprimeTodo();
+        Contacto c1 = new Contacto("Braulio", "UASLP", 4442222707L);
+        Contacto c2 = new Contacto("Roberto", "UASLP", 4442222707L);
+        Contacto c3 = new Contacto("Fernando", "UASLP", 4442222707L);
+//        Agenda agenda = new Agenda();
+        this.agenda.agregaContacto(c1);
+        this.agenda.agregaContacto(c2);
+        this.agenda.agregaContacto(c3);
+        this.agenda.imprimeTodo();
     }
-
     public void entradaUsuario()
     {
         Scanner entrada = new Scanner(System.in);
-        String opcion= "";
-        /*catch(Exception ex)
+        String opcion = "";
+        do
         {
-            System.out.println(ex.getMessage());
-            opcion="";
-        }*/
-        do try {
-            System.out.println("Opciones: agregar - imprimir - terminar - guardar");
-            opcion = entrada.nextLine();
-            switch (opcion) {
-                case "agregar":
-                    agregar(entrada);
-                    break;
-                case "guardar":
-                    try {
+            try{
+                System.out.println("Opciones: agregar - cargar - imprimir - guardar - terminar");
+                opcion = entrada.nextLine();
+                switch(opcion)
+                {
+                    case "agregar":
+                        agregar(entrada);
+                        break;
+                    case "cargar":
+                        cargar(entrada);
+                        break;
+                    case "imprimir":
+                        agenda.imprimeTodo();
+                        break;
+                    case "guardar":
                         guardar(entrada);
-                        agenda.guardar("archivo.csv");
-                    } catch (IOException e) {
-                        //e.printStackTrace();
-                        System.out.println("Nombre no valido");
-                        opcion="";
-                    }
-                    break;
-                case "imprimir":
-                    agenda.imprimeTodo();
-                    break;
+                        break;
+                    case "terminar":
+                        System.out.println("Nos vemos" +
+                                "");
+                        break;
+                    default:
+                        System.out.println("Opcion no valida");
+                        break;
+                }
             }
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage());
-            opcion = "";
-        } catch (StringIndexOutOfBoundsException ex) {
-            System.out.println(ex.getMessage());
-            opcion = "";
-        } catch (InputMismatchException ex) {
-            System.out.println("El numero debe tener digitos solamente");
-            opcion = "";
-        } while(opcion!="terminar");
+            catch (IllegalArgumentException ex)
+            {
+                System.out.println(ex.getMessage());
+                opcion = "";
+            }
+            catch (InputMismatchException ex2)
+            {
+                System.out.println("El numero de telefono debe tener unicamente digitos");
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.print("El archivo no existe");
+                opcion = "";
+            }
+            catch(StringIndexOutOfBoundsException ex1)
+            {
+                System.out.println(ex1.getMessage());
+                opcion ="";
+            } catch (IOException e) {
+                System.out.print("El nombre del archivo no es valido");
+                opcion = "";
+            }
+        }while(opcion!="terminar");
     }
 
-    private void guardar(Scanner entrada) throws IOException 
+    private void cargar(Scanner entrada) throws FileNotFoundException
     {
-        System.out.println("Nombre del Archivo: ");
+        System.out.print("Nombre del archivo: ");
         String nomArchivo = entrada.nextLine();
-        agenda.guardar(nomArchivo);
+        agenda.cargar(nomArchivo);
     }
 
-    public void agregar(Scanner entrada)
+    private void guardar(Scanner entrada)throws IOException
     {
-        System.out.println("Agregar un contacto: ");
+        System.out.print("Nombre del archivo: ");
+        String nomArchivo = entrada.nextLine();
+        agenda.guardar(nomArchivo + ".csv");
+    }
+
+    private void agregar(Scanner entrada)
+    {
+        System.out.println("Agregar un nuevo contacto");
         System.out.println("Nombre: ");
         String nombre = entrada.nextLine();
         System.out.println("Direccion: ");
-        String direccion= entrada.nextLine();
-        System.out.println("Tel: ");
-        long tel =  entrada.nextLong();
-        Contacto contacto= new Contacto(nombre,direccion,tel);
-        agenda.agregaContacto(contacto);
+        String direccion = entrada.nextLine();
+        System.out.println("Telefono: ");
+        long telefono = entrada.nextLong();
+        Contacto nuevo = new  Contacto(nombre,direccion,telefono);
+        agenda.agregaContacto(nuevo);
     }
-
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        AplicacionTerminal aplicacion =  new AplicacionTerminal();
+        AplicacionTerminal aplicacion = new AplicacionTerminal();
         aplicacion.demo();
         aplicacion.entradaUsuario();
     }
 }
-
